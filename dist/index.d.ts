c9c9a772c5775e5da990577902f1a19b7e878aa6
Zim <zimtsui@gmail.com>
2021-05-02 21:53:50 +0800

    init

---
+++ dist/index.d.ts
@@ -0,0 +1,7 @@
+export declare const kebabCase: RegExp;
+export declare const snakeCase: RegExp;
+export declare const pascalCase: RegExp;
+export declare const camelCase: RegExp;
+export declare const constantCase: RegExp;
+export declare const cobolCase: RegExp;
+export declare const headerCase: RegExp;
