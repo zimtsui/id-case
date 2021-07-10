26e5c0f8e181ff41f812cf421a2d694e676998e6
Zim <zimtsui@gmail.com>
2021-07-10 08:27:33 +0800

    us & renamed

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,8 @@
-export const kebabCase = /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/;
-export const snakeCase = /^([a-z][a-z0-9]*)(_[a-z0-9]+)*$/;
-export const pascalCase = /^[A-Z][a-zA-Z0-9]*$/;
-export const camelCase = /^[a-z][a-zA-Z0-9]*$/;
-export const constantCase = /^([A-Z][A-Z0-9]*)(_[A-Z0-9]+)*$/;
-export const cobolCase = /^([A-Z][A-Z0-9]*)(-[A-Z0-9]+)*$/;
-export const headerCase = /^([A-Z][a-z0-9]*)(-[A-Z0-9][a-z0-9])*$/;
+export const kebabCase = /^([a-z][a-z0-9]*)(-[a-z0-9]+)*$/us;
+export const snakeCase = /^([a-z][a-z0-9]*)(_[a-z0-9]+)*$/us;
+export const pascalCase = /^[A-Z][a-zA-Z0-9]*$/us;
+export const camelCase = /^[a-z][a-zA-Z0-9]*$/us;
+export const constantCase = /^([A-Z][A-Z0-9]*)(_[A-Z0-9]+)*$/us;
+export const cobolCase = /^([A-Z][A-Z0-9]*)(-[A-Z0-9]+)*$/us;
+export const headerCase = /^([A-Z][a-z0-9]*)(-[A-Z0-9][a-z0-9])*$/us;
 //# sourceMappingURL=index.js.map
